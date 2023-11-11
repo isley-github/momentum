f2de63ae9e6deb687b427ec7c70ef75baf4e251a
isley-github <minkyeong.study@gmail.com>
2023-11-12 00:49:37 +0900

    {{delete todo}}

--- js/todo.js
+++ js/todo.js
@@ -4,15 +4,27 @@ const toDoList = document.getElementById("todo-list");
 // form 3번째 줄 ul
 // toDoForm.querySelector("input"); (id가 todo-form인 form에서 input 찾기)
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+//target = 클릭된 HTML element
+// parentElement = 클릭된 element의 상위 li
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
 // li.appendChild(span); = li 내부에 <span></span>이 들어가게 된다.
+// append 나중에 추가
 // span.innerText = newTodo; span의 text로 handleToDoSubmit의 newTodo가 들어간다.
 // event 첫 발생 시 handleToDoSubmit의 첫 번째 인자로 들어감
 
